ce4012bb26065661570c9f9cff7e24329196606d
jwhitlow45 <jwhitlow45@gmail.com>
2022-03-11 23:10:39 -0800

    feat:deck generated when shoe object created

--- Blackjack.py
+++ Blackjack.py
@@ -52,7 +52,10 @@ class Shoe:
         
         # contains all cards for a single deck of cards
         # A = 1, J = 11, Q = 12, K = 13
-        _single_deck = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+        _single_suit = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+        self.deck = []
+        for i in range(numDecks*4):
+            self.deck += _single_suit
         
     
     def shuffle():
