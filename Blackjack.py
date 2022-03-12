230e91000ae0ec9efe554fa21122a001bf0cfa8f
jwhitlow45 <jwhitlow45@gmail.com>
2022-03-11 22:31:46 -0800

    feat:add shoe class

--- Blackjack.py
+++ Blackjack.py
@@ -13,3 +13,12 @@ class Player:
         pass
         
 
+class Shoe:
+    def __init__(self, numDecks: int):
+        
+        # contains all cards for a single deck of cards
+        # A = 1, J = 11, Q = 12, K = 13
+        _single_deck = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+    
+    def shuffle():
+        pass
