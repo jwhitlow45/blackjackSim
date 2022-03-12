ba85c270b7b6c72326563e18a77aeb4275147de3
jwhitlow45 <jwhitlow45@gmail.com>
2022-03-11 22:31:03 -0800

    feat: add blackhack file with game class

---
+++ Blackjack.py
@@ -0,0 +1,7 @@
+from sys import maxsize
+
+class Game:
+    def __init__(self, numDecksInShoe: int, numPlayerStartingChips: int, maxBet: int, numChipsToWin: int = maxsize):
+        pass
+    pass
+
